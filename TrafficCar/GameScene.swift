6df0ddedcb25d8a7b9abedfc4ee62b693b90a004
Richu <richujain1995@gmail.com>
2020-06-18 16:10:40 -0400

    Initial Commit

--- TrafficCar/GameScene.swift
+++ TrafficCar/GameScene.swift
@@ -16,7 +16,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        self.label = self.childNode(withName: "//richu") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
